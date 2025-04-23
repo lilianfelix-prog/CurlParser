c80a5b714c8b5ecdf7f03620b271bf834f0c2a2a
lilianfelix-prog <anyinternal445@DESKTOP-K95280K>
2025-04-23 11:43:22 -0400

    can find tags in the piped html using curl, buffer allocation problem with large html

--- CliParser.c
+++ CliParser.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 1024
 
 void parseHtml(char *input){
-    int start, end = 0;
-    char *content = NULL;
+    int start, end = -1;
     for (int i = 0; i < strlen(input); i++){
         if(input[i] == '<'){
             start = i;
@@ -16,27 +16,44 @@ void parseHtml(char *input){
             end = i;
         }
     }
-    content = (char *)malloc((end - start + 1) * sizeof(char));
-    for(int i = 0; i <= sizeof(content); i++){
-        content[i] = input[start + i];
+
+    int length = end - start + 1;
+
+    if(end != -1 && start != -1){
+
+       char *content = (char *)malloc((length) * sizeof(char));
+
+        strncpy(content, input + start, length);
+       //for(int i = 0; i < length; i++){
+        //    content[i] = input[start + i];
+        //}
+
+        content[length] = '\0';
+        printf("\nParsed content: %s\n", content);
+        free(content); 
     }
-    content[end - start + 1] = '\0';
-    printf("\nParsed content: %s\n", content);
-    free(content);
+    
 }
 
 int main(int argc, char *argv[]){
+    clock_t start, end;
+
+    start = clock();
     char buffer[BUFFER_SIZE];
     int count = 0;
     while(fgets(buffer, BUFFER_SIZE, stdin) != NULL){
         
         buffer[strcspn(buffer, "\n")] = 0;
         count++;
-       // printf("%s", buffer);
+       //printf("%s", buffer);
         parseHtml(buffer);
          
     }
+    end = clock();
+
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nTime taken to parse: %f seconds\n", time_taken);
     printf("\nTotal lines read: %d\n", count);
-            fprintf(stderr, "Error reading input\n");
+            
     return 0;
 }
